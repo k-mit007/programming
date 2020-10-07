26ca6cd554163ae3cf45468065743291a34abbfa
MeetAhya11 <65812177+MeetAhya11@users.noreply.github.com>
2020-10-07 23:04:59 +0530

    Update ArmstrongNumber.py

--- ArmstrongNumber.py
+++ ArmstrongNumber.py
@@ -2,11 +2,11 @@ n=int(input())
 sum=0
 t=n
 while n>0:
-digit=t%10
-sum+=digit**3
-t/=10
+  digit=t%10
+  sum+=digit**3
+  t/=10
 
 if sum==n:
-print("Armstrong")
+  print("Armstrong")
 else:
-print("Not Armstrong)
+  print("Not Armstrong)
